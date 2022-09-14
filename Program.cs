65f55dc2a45f0adfc523a507d2f94c07abb9adc8
aalmathu <aalmathu@LNOR070973>
2022-09-14 12:59:48 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿int index = 0;
+string[] products=new string[10];
+string val="";
+
+Console.WriteLine("Enter Products. Finish by typing Exit");
+
+while (val != "exit")
+{
+   // Console.WriteLine("Add a product");
+    val = Console.ReadLine();
+    if (val == "exit")
+    {
+        break;
+
+    }
+    products[index] = val;
+    index++;
+
+}
+
+Array.Resize(ref products, index);
+Console.WriteLine("You entered following products");
+
+for (index = 0; index < products.Length; index++)
+{
+    Console.WriteLine(products[index]);
+}
+
+
